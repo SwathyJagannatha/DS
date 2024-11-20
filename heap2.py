1062a2297fcd7c4db5286eb216f9e0cdfb26cbb8
SwaTej <tejswa@Mac.attlocal.net>
2024-11-20 10:00:00 -0800

    ds program

---
+++ heap2.py
@@ -0,0 +1,16 @@
+import heapq
+
+# Creating an initial heap
+h = [10, 20, 15, 30, 40]
+heapq.heapify(h)
+
+# Appending an element
+heapq.heappush(h, 5)
+
+# Pop the smallest element from the heap
+min = heapq.heappop(h)
+
+print(h)
+
+print("Smallest:", min)
+print(h)
